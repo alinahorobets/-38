eaac7f4fe3553037132133c3e2f8304e8b9cfb6c
alina horobets <92207382+alinahorobets@users.noreply.github.com>
2022-02-12 13:57:35 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const incomeSalary = document.getElementById('income-salary'),
+      incomeFreelance = document.getElementById('income-freelance'),
+      incomeExtra1 = document.getElementById('income-extra-1'),
+      incomeExtra2 = document.getElementById('income-extra-2'),
+
+      costFlat = document.getElementById('costs-flat'),
+      costHouseServices = document.getElementById('costs-house-services'),
+      costTransport = document.getElementById('costs-transport'),
+      costCredit = document.getElementById('costs-credit'),
+
+      totalMonthInput = document.getElementById('total-month')
+      totalDayInput = document.getElementById('total-day')
+      totalYearInput = document.getElementById('total-year')
+
+    
+let totalDay, totalMonth, totalYear;
+
+const moneyBoxRange = document.getElementById('money-box-range')
+      accumulationInput = document.getElementById('accumulation')
+      totalSpend = document.getElementById('spend')
+
+let accumulation = 0
+let totalpresents = 0
+
+const inputs = document.querySelectorAll('.input')
+
